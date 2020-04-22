4e91f313523e3da9c9a0949263b3b13297cabf4c
eulerfcosta <euler.dacosta@gmail.com>
2020-04-22 01:18:43 -0300

    c005 - Filters

---
+++ pyreststok/core/filters.py
@@ -0,0 +1,89 @@
+from django_filters import rest_framework as filters
+
+from .models Client , Stok_Type, Product_Type, Condominium,Client_Address
+from .models Product, Stok, Item, Stok_Moviment
+
+
+class ClientFilter(filters.FilterSet):
+
+    class Meta:
+        model = Client
+        fields = {
+            'name' : ['icontains'],
+            'doc' : ['icontains']
+        }
+
+class Stok_TypeFilter(filters.FilterSet):
+
+    class Meta:
+        model = Stok_Type
+        fields = {
+            'name' : ['icontains']
+        }
+
+class Product_TypeFilter(filters.FilterSet):
+
+    class Meta:
+        model = Product_Type
+        fields = {
+            'name' : ['icontains']
+        }
+
+class CondominiumFilter(filters.FilterSet):
+
+    class Meta:
+        model = Condominium
+        fields = {
+            'name' : ['icontains'],
+            'address' : ['icontains'],
+            'neighborhood' : ['icontains'],
+            'city' : ['icontains']
+        }
+
+class Client_AddressFilter(filters.FilterSet):
+
+    class Meta:
+        model = Client_Address
+        fields = {
+            'Client',
+            'Condominium'
+        }
+
+class ProductFilter(filters.FilterSet):
+
+    class Meta:
+        model = Product
+        fields = {
+            'name' : ['icontains'],
+            'Product_Type'
+        }
+
+class StokFilter(filters.FilterSet):
+
+    class Meta:
+        model = Stok
+        fields = {
+            'name' : ['icontains'],
+            'Stok_Type'
+        }
+
+class ItemFilter(filters.FilterSet):
+
+    class Meta:
+        model = Item
+        fields = {
+            'Product',
+            'Stok',
+            'User',
+            'sn' : ['icontains']
+        }
+
+class Stok_MovimentFilter(filters.FilterSet):
+
+    class Meta:
+        model = Stok_Moviment
+        fields = {
+            'Item',
+            'Stok',
+            'User'
+        }

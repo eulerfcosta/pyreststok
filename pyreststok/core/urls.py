3acf315642e19488628a1f47f8d9c81aca63874a
eulerfcosta <euler.dacosta@gmail.com>
2020-04-22 01:56:21 -0300

    c009- routes

--- pyreststok/core/urls.py
+++ pyreststok/core/urls.py
@@ -1,8 +1,20 @@
 from django.urls import path
 from rest_framework import routers
 
+from .views import ClientViewSet , Stok_TypeViewSet, Product_TypeViewSet
+from .views import CondominiumViewSet, Client_AddressViewSet, ProductViewSet
+from .views import StokViewSet, ItemViewSet, Stok_MovimentViewSet
+
 
 router = routers.DefaultRouter()
-
+router.register(r'pyreststok/clients', ClientViewSet)
+router.register(r'pyreststok/stokstypes', Stok_TypeViewSet)
+router.register(r'pyreststok/productstypes', Product_TypeViewSet)
+router.register(r'pyreststok/condominiums', CondominiumViewSet)
+router.register(r'pyreststok/clientsaddresses', Client_AddressViewSet)
+router.register(r'pyreststok/products', ProductViewSet)
+router.register(r'pyreststok/stoks', StokViewSet)
+router.register(r'pyreststok/items', ItemViewSet)
+router.register(r'pyreststok/stokmoviments', Stok_MovimentViewSet)
 
 core_urls = router.urls
